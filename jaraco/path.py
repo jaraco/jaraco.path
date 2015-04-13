fb70251cf4f4dbc03860965eb56509f8708ecf1c
Jason R. Coombs <jaraco@jaraco.com>
2015-04-12 21:30:10 -0400

    Fix error in splitext

--- jaraco/path.py
+++ jaraco/path.py
@@ -36,7 +36,7 @@ def __get_numbered_paths(filepath):
 
 def splitext_files_only(filepath):
 	"Custom version of splitext that doesn't perform splitext on directories"
-	return (filepath, '') if os.path.isdir(filepath) else os.path.splitext()
+	return (filepath, '') if os.path.isdir(filepath) else os.path.splitext(filepath)
 
 def set_time(filename, mod_time):
 	"""
