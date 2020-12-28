9f3607798fde5e4251d496f078512346e533bb9a
Jason R. Coombs <jaraco@jaraco.com>
2020-12-27 22:36:54 -0500

    ⚫ Fade to black.

--- tests/test_path.py
+++ tests/test_path.py
@@ -9,41 +9,37 @@ from jaraco import path
 
 
 def test_is_hidden_not(tmpdir):
-	"""
-	A visible directory is not hidden.
-	"""
-	target = six.text_type(tmpdir)
-	assert not path.is_hidden(target)
+    """
+    A visible directory is not hidden.
+    """
+    target = six.text_type(tmpdir)
+    assert not path.is_hidden(target)
 
 
 def test_is_hidden_not_abspath(tmpdir):
-	"""
-	A visible directory, even if referenced by a relative path,
-	should not be considered hidden.
-	"""
-	target = six.text_type(tmpdir) + '/.'
-	assert not path.is_hidden(target)
+    """
+    A visible directory, even if referenced by a relative path,
+    should not be considered hidden.
+    """
+    target = six.text_type(tmpdir) + '/.'
+    assert not path.is_hidden(target)
 
 
 def test_is_hidden():
-	assert path.is_hidden('.hg')
+    assert path.is_hidden('.hg')
 
 
-@pytest.mark.skipif(
-	platform.system() != 'Windows',
-	reason="Windows only")
+@pytest.mark.skipif(platform.system() != 'Windows', reason="Windows only")
 def test_is_hidden_Windows(tmpdir):
-	target = os.path.join(tmpdir, 'test')
-	ctypes.SetFileAttributes(target, 2)
-	assert path.is_hidden(target)
-	assert path.is_hidden_Windows(target)
+    target = os.path.join(tmpdir, 'test')
+    ctypes.SetFileAttributes(target, 2)
+    assert path.is_hidden(target)
+    assert path.is_hidden_Windows(target)
 
 
-@pytest.mark.skipif(
-	platform.system() != 'Darwin',
-	reason="Darwin only")
+@pytest.mark.skipif(platform.system() != 'Darwin', reason="Darwin only")
 def test_is_hidden_Darwin():
-	# cheat because ~/Library is presumably hidden
-	target = os.path.expanduser('~/Library')
-	assert path.is_hidden(target)
-	assert path.is_hidden_Darwin(target)
+    # cheat because ~/Library is presumably hidden
+    target = os.path.expanduser('~/Library')
+    assert path.is_hidden(target)
+    assert path.is_hidden_Darwin(target)
